5001638a0f0494e55bae9fc649e7f235b29164c1
FaragElsayed2 <farag_agoor@mabrains.com>
2022-09-14 18:17:37 +0200

    Cleaning DSM files , ADding openlane flow for DSM

---
+++ pll/DSM_lib/OpenLane/runs/RUN_2022.09.14_15.30.40/config_in.tcl
@@ -0,0 +1,12 @@
+set ::env(PDK) {sky130A}
+set ::env(STD_CELL_LIBRARY) {sky130_fd_sc_hd}
+set ::env(DESIGN_NAME) {DSM}
+set ::env(VERILOG_FILES) {/home/farag/open_design_environment/tools/OpenLane-2022.09.08/designs/DSM/src/Accumulator_tb.v /home/farag/open_design_environment/tools/OpenLane-2022.09.08/designs/DSM/src/DSM_Full_tb.v /home/farag/open_design_environment/tools/OpenLane-2022.09.08/designs/DSM/src/Accumulator.v /home/farag/open_design_environment/tools/OpenLane-2022.09.08/designs/DSM/src/DSM.v /home/farag/open_design_environment/tools/OpenLane-2022.09.08/designs/DSM/src/Special_Adder.v /home/farag/open_design_environment/tools/OpenLane-2022.09.08/designs/DSM/src/Special_Adder_tb.v}
+set ::env(CLOCK_TREE_SYNTH) {0}
+set ::env(CLOCK_PORT) {clk}
+set ::env(CLOCK_NET) {clk}
+set ::env(FP_CORE_UTIL) {15}
+set ::env(SYNTH_MAX_FANOUT) {8}
+set ::env(PL_TARGET_DENSITY) {0.25}
+set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) {0.25}
+set ::env(CLOCK_PERIOD) {25}

b624257931239978fb4345f0f2e0257259b29f81
ME <ahmedalshahat86@gmail.com>
2022-09-20 09:44:41 +0000

    PDK oscillator update 1

---
+++ pll/lc_vco_lib_Ahmed_Elshahat/CMOS_LC_VCO/PDK_varactor/scripts/plot_fosc.py
@@ -0,0 +1,7 @@
+import sys
+import numpy as np
+
+file_name  = sys.argv[1]   #file name of the netlist subscribes the circuit
+counter = sys.argv[2]        #expresion of the output voltage 
+
+for i in range (0,counter)

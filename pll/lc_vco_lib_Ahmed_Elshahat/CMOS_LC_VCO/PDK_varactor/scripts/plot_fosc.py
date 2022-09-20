1046928243198a4fedabca8c021b383cb24a35af
ME <ahmedalshahat86@gmail.com>
2022-09-20 09:52:37 +0000

    PDK oscillator update 2

--- pll/lc_vco_lib_Ahmed_Elshahat/CMOS_LC_VCO/PDK_varactor/scripts/plot_fosc.py
+++ pll/lc_vco_lib_Ahmed_Elshahat/CMOS_LC_VCO/PDK_varactor/scripts/plot_fosc.py
@@ -1,7 +1,27 @@
 import sys
 import numpy as np
+import pandas as pd
 
-file_name  = sys.argv[1]   #file name of the netlist subscribes the circuit
+def get_mag_arr(file_name):
+	file = pd.read_csv (file_name,index_col=False,usecols=[1],header=None, delimiter=r"\s+")
+	array = file.to_numpy()
+	array = array.ravel()
+	array = np.array(array)
+	return array
+
+def get_freq_arr(file_name):
+	file = pd.read_csv (file_name,index_col=False,usecols=[0],header=None, delimiter=r"\s+")
+	array = file.to_numpy()
+	array = array.ravel()
+	array = np.array(array)
+	return array
+
+
+file_name1 = sys.argv[1]   #file name of the netlist subscribes the circuit
 counter = sys.argv[2]        #expresion of the output voltage 
 
-for i in range (0,counter)
+for i in range (0,int(counter)):
+    counter_str = str(i)
+    file_name = file_name1+counter_str
+
+    print(file_name)

4e44b0437f55a8b3ca8fb2876d33583e4e3b2f90
Mmoatazz <91559822+Mmoatazz@users.noreply.github.com>
2022-09-20 15:24:43 +0200

    -Adding Python Script for plotting results

--- pll/system/ngspice_time_model/test_benches/run_pfd_tb.py
+++ pll/system/ngspice_time_model/test_benches/run_pfd_tb.py
@@ -1,210 +1,78 @@
 ################################################################################################
-
 ## Mabrains LLC
-
 ################################################################################################
-
-
-
 from genericpath import isfile
-
 import subprocess
-
 import os
-
 import pandas as pd
-
 import matplotlib.pyplot as plt
 
-
-
 plt.rcParams["figure.figsize"] = (12, 10)
-
 plt.rcParams["figure.autolayout"] = True
 
-
-
-
-
 if __name__ == "__main__":
-
     
-
-    output_file = "PFD_behave.csv"
-
-
-
+    output_file = "pfd_behave.csv"
     ## Delete existing output file
 
     if os.path.isfile(output_file):
 
         os.remove(output_file)
 
-
-
     ## Run the simulation
 
-    #subprocess.run(["ngspice", "-b", "-a", "PFD_TB.sp", "-o", "PFD_log_file.log"])
-
-    subprocess.run(["ngspice", "-b", "PFD_TB.sp"])
+    subprocess.run(["ngspice", "-b", "test_pfd.sp"])
 
     if not os.path.isfile(output_file):
 
         print("## Simulation didn't complete.")
-
         exit()
-
-    
-
-
-
-    fields = ['time', 'v(S_FB)','v(S_REF)','v(UP_SIG)','v(DWN_SIG)']
-
-    
-
+    #import the signal arrays
+    fields = ['time', 'v(FBS)','v(REFS)','v(UP)','v(DWN)']
     data_df = pd.read_csv(output_file,sep='\s+')
-
     timearr = data_df['time']
-
-    FB      = data_df['v(S_FB)']    
-
-    REF     = data_df['v(S_REF)']  
-
-    UP      = data_df['v(UP_SIG)']
-
-    DWN     = data_df['v(DWN_SIG)']        
+    FB      = data_df['v(FBS)']    
+    REF     = data_df['v(REFS)']  
+    UP      = data_df['v(UP)']
+    DWN     = data_df['v(DWN)']        
 
     plt.figure(1)
 
-
-
+    #Ref Signal
     plt.subplot(4,1,1)
-
-
-
     plt.plot(timearr , REF , color='b', label='REF')
-
-
-
     plt.xlabel('time (sec)') 
-
-
-
     plt.ylabel('Amplitude (V)')
-
-
-
     plt.title('REF')
-
-    
-
     plt.xlim(4e-6, 4.2e-6)
-
-
-
     plt.grid(True)
-
-
-
-
-
+    
+    #FB Signal
     plt.subplot(4,1,2)
-
-    
-
-
-
     plt.plot(timearr , FB, color='g', label='FB')
-
-
-
     plt.xlabel('time (sec)') 
-
-
-
     plt.ylabel('Amplitude (V)')
-
-
-
     plt.title('FB')
-
-    
-
     plt.xlim(4e-6, 4.2e-6)
-
-    
-
     plt.grid(True)
-
     
-
+    #UP Signal
     plt.subplot(4,1,3)
-
-
-
     plt.plot(timearr , UP ,color='r', label='UP')
-
-
-
     plt.xlabel('time (sec)') 
-
-
-
     plt.ylabel('Amplitude (V)')
-
-
-
     plt.title('UP')
-
-    
-
     plt.xlim(4e-6, 4.2e-6)
-
-
-
     plt.grid(True)
-
-
-
+    
+    #DWN Signal
     plt.subplot(4,1,4)
-
-
-
     plt.plot(timearr , DWN , color='c', label='DWN')
-
-
-
-    plt.xlabel('time (sec)') 
-
-
-
+    plt.xlabel('time (sec)')
     plt.ylabel('Amplitude (V)')
-
-
-
     plt.title('DWN')
-
-    
-
     plt.xlim(4e-6, 4.2e-6)
-
-
-
     plt.grid(True)
-
-
-
-    plt.title("DWN")
-
-
 
     plt.legend()
-
-
-
     plt.show()
-
-
-
-    
-

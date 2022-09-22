c8d2a72fc9d972bd4ea6816790057e8fee3e6aef
ME <ahmedalshahat86@gmail.com>
2022-09-22 07:36:57 +0000

    changing the current source direction and add comments to the main python scripy

--- pll/selected_vco_lib/scripts/run_vco_corners.py
+++ pll/selected_vco_lib/scripts/run_vco_corners.py
@@ -12,11 +12,14 @@ import itertools
 import concurrent.futures
 import shutil
 
-main_tb_path = os.path.join("..", "spice_files")
+# get he path of the folder which contain the tb 
+main_tb_path = os.path.join("..", "spice_files") 
+
+# get the directory of the run folder which contain the log and tb files for each corner
 run_dir = os.path.join("..", "run_test")
 
-TEMPLATE_FILE = "test_vco_char.spice"
-NUM_WORKERS = 5
+TEMPLATE_FILE = "test_vco_char.spice" #name of the tb 
+NUM_WORKERS = 5 # maximum number of processor threds to operate on 
 
 process_corners = ["ss", "sf", "fs", "ff", "tt"]
 temp_corners = [-40, 27, 125]
@@ -25,6 +28,7 @@ vctrl_corners = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.9, 1.0, 1.1, 1.2, 1.3
 
 supply_value = 1.8
 
+# create a string to carry all the lines related to the corners
 corner_str = """
 .lib /open_design_environment/foundry/pdks/skywaters/sky130A/libs.tech/ngspice/sky130.lib.spice {corner}
 .temp {temp}
@@ -32,32 +36,42 @@ corner_str = """
 
 VDD VDD GND {vsup}
 VTuner vctrl GND {vctrl}
-Isource ibias VDD 90u"""
+Isource VDD ibias 90u"""
 
 ## .nodeset v(vout)=0
 
 def run_corner(all_corner_data):
+    # This function gets a list crries al the corner cases and returns a list of 
+    # values of all the intended measurments
+
     templateLoader = jinja2.FileSystemLoader(searchpath=main_tb_path)
     templateEnv = jinja2.Environment(loader=templateLoader)
     template = templateEnv.get_template(TEMPLATE_FILE)
 
+    # extract the (process,temp,supply,control voltage) values
+    # from the input list to update
     pc = all_corner_data[0]
     tc = "{:.2f}".format(all_corner_data[1])
     sc = "{:.2f}".format(all_corner_data[2] * supply_value)
     vc = "{:.2f}".format(all_corner_data[3])
 
+    # updatet the corner lines with the values of the intended corner
     new_corners_str = corner_str.format(corner=pc, 
                                         temp=tc, 
                                         vsup=sc, 
                                         vctrl=vc)
 
+    # update the tb with the new values and save the content in a variable
     full_spice = template.render(corner_setup=new_corners_str)
 
+    # create a new tb for the intended corner and update it and then close it
     spice_file_path = os.path.join(run_dir, "{}_{}_{}_{}.spi".format(pc, tc, sc, vc))
     text_file = open(spice_file_path, "w")
     text_file.write(full_spice)
     text_file.close()
 
+    # create a log file for the intended corner and 
+    # then run the tb 
     spice_run_log = os.path.join(run_dir, "{}_{}_{}_{}.log".format(pc, tc, sc, vc))
     log_file = open(spice_run_log, "w")
     subprocess.run(["ngspice", "-b", spice_file_path], stdout=log_file, stderr=log_file)
@@ -65,8 +79,13 @@ def run_corner(all_corner_data):
 
     ## read the data from log
     log_file = open(spice_run_log, "r")
-    results_dict = {}
 
+    #create an empty list to save the measurements
+    # this list has labels of the name of the  measurement
+    # you can append whatever you want of measurments
+    results_dict = {} 
+
+    #iterate on the log file and extract the values of the intended measurments
     for line in log_file.readlines():
         s = line.split()
         if len(s) > 2:
@@ -75,20 +94,26 @@ def run_corner(all_corner_data):
             elif s[0] == "vn":
                 results_dict["vn"] = s[2]
 
-    log_file.close()
+    log_file.close() # close the log file
 
-
+    # return the list carrying the measurments
     return results_dict
 
+
+
 if __name__ == "__main__":
-    
+    # create a list has all the combinations of corner cases
     all_comb = list(itertools.product(process_corners, temp_corners, supply_corners, vctrl_corners))
 
+    # if the run folder is not found, create a new folder with the givven path which is 
+    # created at the beginning of the script
     if not os.path.isdir(run_dir):
         os.makedirs(run_dir)
     
+    # copy the spiceinit file to the run folder so there is comaptibility mode during the simulation
     shutil.copyfile("/open_design_environment/foundry/pdks/skywaters/sky130A/libs.tech/ngspice/spinit", os.path.join(run_dir, ".spiceinit"))
     
+    # create an empty list to carry all the measurements for all the corners
     my_results = []
 
     # We can use a with statement to ensure threads are cleaned up promptly
@@ -99,11 +124,16 @@ if __name__ == "__main__":
         for future in concurrent.futures.as_completed(future_to_comb):
             comb = future_to_comb[future]
             try:
-                data = future.result()
+                data = {}
                 data["process"] = comb[0]
                 data["temp"] = comb[1]
                 data["supply"] = comb[2]
                 data["control"] = comb[3]
+                
+                new_data = future.result()
+
+                data.update(new_data)
+                
                 my_results.append(data)
 
             except Exception as exc:
@@ -114,5 +144,6 @@ if __name__ == "__main__":
 
     if len(my_results) > 0:
         df = pd.DataFrame(my_results)
+        df.sort_values(by="control", inplace=True)
         df.to_csv("all_measurements.csv", index=False)
     

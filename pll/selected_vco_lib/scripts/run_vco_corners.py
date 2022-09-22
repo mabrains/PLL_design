9301f9f80afd1d85d38e7a049b6e174a3c3f23a1
ME <ahmedalshahat86@gmail.com>
2022-09-22 16:43:16 +0000

    fetching the failed corners

--- pll/selected_vco_lib/scripts/run_vco_corners.py
+++ pll/selected_vco_lib/scripts/run_vco_corners.py
@@ -196,3 +196,16 @@ if __name__ == "__main__":
         df.sort_values(by="control", inplace=True)
         df.to_csv("all_measurements.csv", index=False)
     
+    failed_corners = []
+    for ind in df.index:
+        if df['Oscillation Status'][ind] == False:
+            corner = {}
+            corner["failed_corners"] = df['process'][ind]+'_'+str(df['temp'][ind])+'_'+str(df['supply'][ind])
+            if (corner not in failed_corners ):
+             failed_corners.append(corner)
+
+
+
+    if len(failed_corners) > 0:
+        df = pd.DataFrame(failed_corners)
+        df.to_csv("failed_corners.csv", index=False)

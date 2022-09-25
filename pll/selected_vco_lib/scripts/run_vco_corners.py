96d6a4e4443ad2bbe7b50816c9be14549a6e8bf9
ME <ahmedalshahat86@gmail.com>
2022-09-25 13:13:27 +0000

    the design is modified

--- pll/selected_vco_lib/scripts/run_vco_corners.py
+++ pll/selected_vco_lib/scripts/run_vco_corners.py
@@ -30,17 +30,17 @@ main_tb_path = os.path.join("..", "spice_files")
 run_dir = os.path.join("..", "run_test")  
 
 TEMPLATE_FILE = "test_vco_char.spice" #name of the tb 
-NUM_WORKERS = 5 # maximum number of processor threds to operate on 
+NUM_WORKERS = 6 # maximum number of processor threds to operate on 
 '''
 process_corners = ["ss", "sf", "fs", "ff", "ss"]
 temp_corners = [-40, 27, 125]
 supply_corners = [0.9, 1.0, 1.1]
 vctrl_corners = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8]
 '''
-process_corners = ["ss", "sf","tt"]
-temp_corners = [-40]
-supply_corners = [0.9]
-vctrl_corners = [0.0,]
+process_corners =  ["tt"]
+temp_corners = [27]
+supply_corners = [1]
+vctrl_corners =  [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8]
 
 supply_value = 1.8
 
@@ -214,8 +214,7 @@ if __name__ == "__main__":
     if len(my_results) > 0:
         df = pd.DataFrame(my_results)
         df.sort_values(by=["corner name","control"] , inplace=True)
-        df.to_csv("all_measurements.csv", index=False)
-'''
+        
     # plotting the passed corners
         for itr in range(0,len(df["control"])-len(vctrl_corners)+1,len(vctrl_corners)):
             control_list = df["control"][itr:itr+len(vctrl_corners)-1].tolist()
@@ -230,6 +229,7 @@ if __name__ == "__main__":
         plt.legend()
         plt.show()
 
-'''
+        df.to_csv("all_measurements.csv", index=False)
+
         
     

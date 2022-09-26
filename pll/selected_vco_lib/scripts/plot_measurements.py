50ea06bb10efa1fa913abd87f62139bbc35fe7cb
ME <ahmedalshahat86@gmail.com>
2022-09-26 09:05:42 +0000

    adding the results of the vco ideal cs with 2 failed corners

--- pll/selected_vco_lib/scripts/plot_measurements.py
+++ pll/selected_vco_lib/scripts/plot_measurements.py
@@ -13,21 +13,20 @@ def D(xlist,ylist):
 
 
 vctrl_corners = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8]
+df = pd.read_csv('all_measurements_original.csv')
+
 fig, ax = plt.subplots(figsize=(4, 4))
 plt.subplots_adjust(right=0.9)
 plt.subplots_adjust(left=0.125)
 plt.subplots_adjust(top=0.88)
 plt.subplots_adjust(bottom=0.07)
 
-df = pd.read_csv('all_measurements.csv')
-
-
 for itr in range(0,len(df["control"])-len(vctrl_corners)+1,len(vctrl_corners)):
-    print (itr)
     control_list = df["control"][itr:itr+len(vctrl_corners)-1].tolist()
     freq_list = df["freq (GHZ)"][itr:itr+len(vctrl_corners)-1].tolist()
     oscilation_state = df["Oscillation Status"][itr:itr+len(vctrl_corners)-1].tolist()
-    plt.plot(control_list , freq_list,linewidth = 2.5,label=df["corner name"][itr])
+    if (False not in oscilation_state) :
+        plt.plot(control_list , freq_list,linewidth = 2.5,label=df["corner name"][itr])
 
 
 plt.legend(bbox_to_anchor=(1.12, 1.13),loc='upper right', labelspacing=0.15)
@@ -38,8 +37,8 @@ plt.grid()
 plt.tight_layout()
 plt.plot(control_list , [2.35]*len(control_list),linewidth=4 ,color='black')
 plt.plot(control_list , [2.55]*len(control_list),linewidth=4 ,color='black')
-#plt.xlim([0, 1])
-plt.ylim([2.3 , 2.6])
+plt.xlim([0, 1.1])
+#plt.ylim([2.3 , 2.6])
 plt.show()
 
 
@@ -50,11 +49,12 @@ plt.subplots_adjust(top=0.88)
 plt.subplots_adjust(bottom=0.07)
 
 for itr in range(0,len(df["control"])-len(vctrl_corners)+1,len(vctrl_corners)):
-    print (itr)
     control_list = df["control"][itr:itr+len(vctrl_corners)-1].tolist()
     freq_list = df["freq (GHZ)"][itr:itr+len(vctrl_corners)-1].tolist()
     control_list_prime, kvco = D(control_list, freq_list)
-    plt.plot(control_list_prime , kvco*1e+3,linewidth = 2.5,label=df["corner name"][itr])
+    oscilation_state = df["Oscillation Status"][itr:itr+len(vctrl_corners)-1].tolist()
+    if (False not in oscilation_state) :
+        plt.plot(control_list_prime , kvco*1e+3,linewidth = 2.5,label=df["corner name"][itr])
 
 
 plt.legend(bbox_to_anchor=(1.12, 1.13),loc='upper right', labelspacing=0.15)
@@ -63,6 +63,6 @@ plt.ylabel("KVCO (MHZ/V)",fontsize=14)
 plt.title("Gain  Vs Control Voltage",fontsize=14)
 plt.grid()
 plt.tight_layout()
-plt.xlim([0, 1])
-plt.ylim([0 , 800])
+plt.xlim([0, 1.1])
+#plt.ylim([0 , 800])
 plt.show()

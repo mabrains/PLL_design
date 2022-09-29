20d3f9582285bc0defd17bca905f9af219fa62ad
reda315 <ahmedreda8178@gmail.com>
2022-09-29 12:19:04 +0200

    Adding divider and dsm integration model

--- pll/divider_and_DSM_model/Div_DSM.py
+++ pll/divider_and_DSM_model/Div_DSM.py
@@ -2,17 +2,18 @@ import numpy as np
 import pandas as pd
 
 NumberofSamples = 2**16
-Bus_Size = 12                            # Bits
-Accumulator_Bits = 12                    # Bits
+Bus_Size = 24                            # Bits
+Accumulator_Bits = 24                    # Bits
 Accumulator_Size = 2**Accumulator_Bits  
 
+input = [0] * Accumulator_Size
 Div_avg = [0] * Accumulator_Size
 Divider = [256,255,254,253,252,251,250,249,248,247,246,245,244,243,242,241,240,239,238,237,236,235,234,233,232,231,230,229,228,227,226,225]
 
 #Fraction = 0.2                              # From 0 to 1
 #Fraction_Internal = (2**Bus_Size)*Fraction
-input = [0] * Accumulator_Size
-for Fraction_Internal in range (0,Accumulator_Size):                      # Choosing channel aka choosing the fraction to divide by
+
+for Fraction_Internal in range (1,200):                      # Choosing channel aka choosing the fraction to divide by
 	input[Fraction_Internal] = Fraction_Internal
 	C1 = [0] * NumberofSamples                 # Carry out of the 1st accumulator
 	C2 = [0] * NumberofSamples                 # Carry out of the 2nd accumulator
@@ -20,13 +21,10 @@ for Fraction_Internal in range (0,Accumulator_Size):                      # Choo
 	U1 = [0] * NumberofSamples                 # Output of the 1st accumulator
 	U2 = [0] * NumberofSamples                 # Output of the 2nd accumulator 
 	U3 = [0] * NumberofSamples                 # Output of the 3rd accumulator
-	#### Output to the divider in 3 cases ####
-	Yout1 = [0] * NumberofSamples              # 1st Order MASH DSM 
-	Yout2 = [0] * NumberofSamples              # 2nd Order MASH DSM
 	Yout3 = [0] * NumberofSamples              # 3rd Order MASH DSM
 	#### Core operation of the DSM ####
 	for index in range(2,NumberofSamples-1):
-		U1[index] = Fraction_Internal + U1[index-1]
+		U1[index] = input[Fraction_Internal] + U1[index-1]
 		U2[index] = U1[index-1] + U2[index-1]
 		U3[index] = U2[index-1] + U3[index-1]
 		if U1[index] > Accumulator_Size:
@@ -38,9 +36,7 @@ for Fraction_Internal in range (0,Accumulator_Size):                      # Choo
 		if U3[index] > Accumulator_Size:
 			C3[index] = 1
 			U3[index] = U3[index] - Accumulator_Size
-			
-		Yout1[index] = C1[index]
-		Yout2[index] = C1[index] + C2[index] - C2[index-1]
+
 		Yout3[index] = C1[index] + C2[index] + C3[index] - C2[index-1] - 2*C3[index-1] + C3[index-2]
 		#print (Yout3[index])
 
@@ -71,9 +67,9 @@ for Fraction_Internal in range (0,Accumulator_Size):                      # Choo
 	for i in range(0,len(DSM_out)):
 		Div_out[i] = Divider[DSM_out[i]]
 	Div_avg[Fraction_Internal] = np.mean(Div_out)
+	print('Input = ',input[Fraction_Internal],' , Div = ',Div_avg[Fraction_Internal])
 
-
-
+'''
 dict = {}
 dict['Input'] = input
 print(len(input))
@@ -81,3 +77,4 @@ print(len(Div_avg))
 dict['Division Ratio'] = Div_avg
 df = pd.DataFrame(dict)
 df.to_csv("Measurements.csv",index=False)
+'''

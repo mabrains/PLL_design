0c4e4cd2992a762158bffeea7f6899c649a1964f
ME <ahmedalshahat86@gmail.com>
2022-10-02 15:49:06 +0000

    uploading the results of the unbalanced inductor

--- pll/selected_vco_lib/scripts/plot_measurements.py
+++ pll/selected_vco_lib/scripts/plot_measurements.py
@@ -13,7 +13,7 @@ def D(xlist,ylist):
 
 
 vctrl_corners = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8]
-df = pd.read_csv('all_measurements.csv')
+df = pd.read_csv('all_measurements_after_replacing_mult_with_nf_unbalanced_right.csv')
 
 fig, ax = plt.subplots(figsize=(8, 10))
 plt.subplots_adjust(right=0.9)
@@ -38,11 +38,11 @@ plt.ylabel("Oscillation Frequency (HZ)",fontsize=14)
 plt.title("Oscillation Frequency Vs Control Voltage",fontsize=14)
 plt.grid()
 plt.tight_layout()
-plt.plot(control_list , [2.35]*len(control_list),linewidth=4 ,color='black')
-plt.plot(control_list , [2.55]*len(control_list),linewidth=4 ,color='black')
+plt.plot(control_list , [2.35]*len(control_list),linewidth=4 ,color='blue')
+plt.plot(control_list , [2.55]*len(control_list),linewidth=4 ,color='blue')
 plt.plot(control_list , [2.4]*len(control_list),linewidth=4 ,color='black')
 plt.plot(control_list , [2.5]*len(control_list),linewidth=4 ,color='black')
-#plt.xlim([0, 1])
+plt.xlim([0, 1.25])
 #plt.ylim([2.3 , 2.6])
 plt.show()
 
@@ -76,7 +76,7 @@ plt.ylabel("KVCO (MHZ/V)",fontsize=14)
 plt.title("Gain  Vs Control Voltage",fontsize=14)
 plt.grid()
 plt.tight_layout()
-#plt.xlim([0, 1])
+plt.xlim([0, 1.25])
 #plt.ylim([0 , 800])
 plt.show()
 
@@ -106,6 +106,37 @@ plt.ylabel("Differential Swing (V)",fontsize=14)
 plt.title("Differntial Swing Vs Control Voltage",fontsize=14)
 plt.grid()
 plt.tight_layout()
-#plt.xlim([0, 1])
+plt.xlim([0, 1.25])
+#plt.ylim([2.3 , 2.6])
+plt.show()
+
+
+## POWER PLOTTING
+
+fig, ax = plt.subplots(figsize=(8, 10))
+plt.subplots_adjust(right=0.9)
+plt.subplots_adjust(left=0.079)
+plt.subplots_adjust(top=0.88)
+plt.subplots_adjust(bottom=0.086)
+
+for itr in range(0,len(df["control"])-len(vctrl_corners)+1,len(vctrl_corners)):
+    control_list = df["control"][itr:itr+len(vctrl_corners)-1].tolist()
+    power_list = df["Power (mW)"][itr:itr+len(vctrl_corners)-1].tolist()
+    #print(swing_list)
+    oscilation_state = df["Oscillation Status"][itr:itr+len(vctrl_corners)-1].tolist()
+    if (False not in oscilation_state) :
+        if (df["corner name"][itr] == 'tt,27,1.0'):
+            plt.plot(control_list , power_list,linewidth = 3, color = 'black',linestyle='dashed',label=df["corner name"][itr])
+        else:
+            plt.plot(control_list , power_list,linewidth = 2.5,label=df["corner name"][itr])
+
+
+plt.legend(bbox_to_anchor=(1.12, 0.95),loc='upper right', labelspacing=0.15)
+plt.xlabel("Control Voltage (V)",fontsize=14)
+plt.ylabel("Power (mW)",fontsize=14)
+plt.title("VCO Power Vs Control Voltage",fontsize=14)
+plt.grid()
+plt.tight_layout()
+plt.xlim([0, 1.25])
 #plt.ylim([2.3 , 2.6])
 plt.show()

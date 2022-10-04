c5ff9fee5c5f6e6a8d82d54becfd408cb610e412
AhmedBadry10 <badryahmed33@gmail.com>
2022-10-04 10:54:00 +0000

    update six

--- pll/pfd_cp_lf_lib/corners/scripts/plot_measurements.py
+++ pll/pfd_cp_lf_lib/corners/scripts/plot_measurements.py
@@ -8,19 +8,39 @@ import numpy as np
 import glob
 import os
 
-files_dir = glob.glob('../csv_files/pfd_ckt_**_10n_0.csv', 
-                   recursive = True)
+files_dir = glob.glob('../csv_files/pfd_ckt_**_10n_0.csv', recursive = True)
+
+
+fig, ax = plt.subplots(figsize=(8, 10))
+plt.subplots_adjust(right=0.9)
+plt.subplots_adjust(left=0.079)
+plt.subplots_adjust(top=0.88)
+plt.subplots_adjust(bottom=0.086)
 
 for file_dir in files_dir:
-
-    print(file_dir)
+    csv_file_dir = file_dir
     file_name = os.path.basename(file_dir)
     corner_name = file_name[8:len(file_name)-10]
-    print (file_name)
-    print (corner_name)
 
+    df = pd.read_csv (csv_file_dir,index_col=False,usecols=[0 ,1 ,2, 3,4],header=0, delimiter=r"\s+")
+    df.to_csv('../csv_files/temp.csv', index = False)
 
+    time_list = df["time"]
+    up_list = df["v(up)"]
 
+    if (corner_name == 'tt_27.00_1.80'):
+        print("tt")
+        plt.plot(time_list , up_list,linewidth = 3, color = 'black',linestyle='dashed',label=corner_name)
+    else:
+        plt.plot(time_list , up_list,linewidth = 2.5,label=corner_name)
+            
+plt.legend(bbox_to_anchor=(1.12, 1.1),loc='upper right', labelspacing=0.15)
+plt.xlabel("Time (sec)",fontsize=14)
+plt.ylabel("Amplitude (V)",fontsize=14)
+plt.title("UP signal Vs Time",fontsize=14)
+plt.grid()
+plt.tight_layout()
+plt.show()
 '''
 csv_file_name = '../csv_files/pfd_ckt_tt_-40.00_1.80_10n_0.csv'
 df = pd.read_csv (csv_file_name,index_col=False,usecols=[0 ,1 ,2, 3,4],header=0, delimiter=r"\s+")

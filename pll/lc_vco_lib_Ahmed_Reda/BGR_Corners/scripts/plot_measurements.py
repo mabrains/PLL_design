d50f0a775c7aeb1c63128f32053657c020bcf19b
ME <ahmedalshahat86@gmail.com>
2022-10-13 17:31:15 +0000

    comparing before and after PEX

--- pll/lc_vco_lib_Ahmed_Reda/BGR_Corners/scripts/plot_measurements.py
+++ pll/lc_vco_lib_Ahmed_Reda/BGR_Corners/scripts/plot_measurements.py
@@ -10,7 +10,11 @@ import matplotlib.pyplot as plt
 current_min = 70
 failed_corner =[]
 
-df = pd.read_csv('all_measurements_PEX_2.csv')
+#####################
+#schematic#######
+###########
+
+df = pd.read_csv('all_measurements_schematic.csv')
 
 # fetching the failed corners
 for index, row in df.iterrows():
@@ -19,7 +23,39 @@ for index, row in df.iterrows():
 
 # saving the failed corners in a separate list
 df_failed = pd.DataFrame(failed_corner)
-df_failed.to_csv("all_measurements_failed_corners.csv", index=False)
+df_failed.to_csv("all_measurements_failed_corners_schematic.csv", index=False)
+
+
+fig, ax = plt.subplots(figsize=(8, 10))
+plt.subplots_adjust(right=0.9)
+plt.subplots_adjust(left=0.055)
+plt.subplots_adjust(top=0.88)
+plt.subplots_adjust(bottom=0.086)
+
+plt.scatter(df["corner name"], df['iref'], s = 50)
+plt.xticks([]) 
+#plt.xticks(rotation = 90)
+plt.xlabel("Corners",fontsize=14)
+plt.ylabel("Refrence Current (uA)",fontsize=14)
+plt.title("BGR Current Before PEX",fontsize=14)
+plt.grid()
+plt.tight_layout()
+plt.plot(df["corner name"] , [70]*len(df["corner name"]),linewidth=4 ,color='blue')
+plt.show()
+
+#####################
+#after pex#######
+###########
+df = pd.read_csv('all_measurements_PEX.csv')
+
+# fetching the failed corners
+for index, row in df.iterrows():
+    if (row["iref"]< current_min):
+        failed_corner.append(row)
+
+# saving the failed corners in a separate list
+df_failed = pd.DataFrame(failed_corner)
+df_failed.to_csv("all_measurements_failed_corners_PEX.csv", index=False)
 
 
 fig, ax = plt.subplots(figsize=(8, 10))
@@ -38,5 +74,3 @@ plt.grid()
 plt.tight_layout()
 plt.plot(df["corner name"] , [70]*len(df["corner name"]),linewidth=4 ,color='blue')
 plt.show()
-
-

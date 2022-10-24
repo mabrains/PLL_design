960b8f9b45917c48634839138d7ca997e6d62027
Mmoatazz <m.mokhtar.8898@gmail.com>
2022-10-24 15:01:05 +0000

    uploading files from the server

---
+++ pll/Integeration/circuit_integeration/tests/test pfd/run_pfd_tb.py
@@ -0,0 +1,118 @@
+################################################################################################
+## Mabrains LLC
+################################################################################################
+
+from genericpath import isfile
+import subprocess
+import pandas as pd
+import matplotlib.pyplot as plt
+from jinja2 import Template
+import os
+import concurrent.futures
+
+
+netlist_tmp = "PFD.sp"
+
+############################# VAR TO NETLIST ##############################
+
+delayup  = 0
+delaydwn = 0
+
+for i in range (0,3):
+    delayup  = 2e-9 if i == 1 else 0
+    delaydwn = 2e-9 if i == 2 else 0
+    with open(netlist_tmp) as f:
+        tmpl = Template(f.read())
+        file = f"test_pfd_mod_{i}.sp" 
+        with open(file, "w") as netlist:
+            netlist.write(tmpl.render(delayup = delayup, delaydwn = delaydwn)) 
+
+# # Running ngspice for each netlist 
+# with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
+#     executor.submit(call_simulator, netlist)
+
+####################################### PLOT ##################################
+
+plt.rcParams["figure.figsize"] = (12, 10)
+plt.rcParams["figure.autolayout"] = True
+
+
+if __name__ == "__main__":
+    
+    for i in range (0,3):
+        if i == 0:
+            file = "test_pfd_mod_0.sp"
+            title_lag_lead = 'Locked State'
+            savefig = 'Locked State.png'
+        elif i == 1:
+            file = "test_pfd_mod_1.sp"
+            title_lag_lead = 'Feedback signal leads the Reference signal'
+            savefig = 'Feedback signal leads the Reference.png'
+        elif i == 2:
+            file = "test_pfd_mod_2.sp"
+            title_lag_lead = 'Reference signal leads the Feedback signal'
+            savefig = 'Reference signal leads the Feedback signal.png'
+            
+        output_file = "pfd_behave.csv"
+
+        ## Delete existing output file
+        if os.path.isfile(output_file):
+            os.remove(output_file)
+
+        ## Run the simulation
+        #subprocess.run(["ngspice", "-b", "-a", "PFD_TB.sp", "-o", "PFD_log_file.log"])
+        print (i)
+        subprocess.run(["ngspice", "-b", file])
+        if not os.path.isfile(output_file):
+            print("## Simulation didn't complete.")
+            exit()
+        
+
+        fields = ['time', 'v(FB)','v(REF)','v(UP)','v(DN)']
+        
+        data_df = pd.read_csv(output_file,sep='\s+')
+        timearr = data_df['time']
+        FB      = data_df['v(FB)']    
+        REF     = data_df['v(REF)']  
+        UP      = data_df['v(UP)']
+        DWN     = data_df['v(DN)']    
+            
+        plt.figure(i+1)
+
+        plt.subplot(4,1,1)
+        plt.plot(timearr , REF , color='b', label='REF')
+        plt.xlabel('time (sec)') 
+        plt.ylabel('Amplitude (V)')
+        plt.title('REF')
+    
+        plt.grid(True)
+
+        plt.subplot(4,1,2)
+        plt.plot(timearr , FB, color='g', label='FB')
+        plt.xlabel('time (sec)') 
+        plt.ylabel('Amplitude (V)')
+        plt.title('FB')
+    
+        plt.grid(True)
+        
+        plt.subplot(4,1,3)
+        plt.plot(timearr , UP ,color='r', label='UP')
+        plt.xlabel('time (sec)') 
+        plt.ylabel('Amplitude (V)')
+        plt.title('UP')
+    
+        plt.grid(True)
+        
+        plt.subplot(4,1,4)
+        plt.plot(timearr , DWN , color='c', label='DWN')
+        plt.xlabel('time (sec)') 
+        plt.ylabel('Amplitude (V)')
+        plt.title('DWN')
+    
+        plt.grid(True)
+
+        plt.legend()
+        #plt.show()
+        
+        plt.suptitle(title_lag_lead)
+        plt.savefig(savefig)

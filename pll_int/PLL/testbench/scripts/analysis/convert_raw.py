7fbf20febbb948d96ff2d9bb180de7cf1a9def90
ME <ahmedalshahat86@gmail.com>
2022-11-01 16:07:00 +0000

    running full pll

--- pll_int/PLL/testbench/scripts/analysis/convert_raw.py
+++ pll_int/PLL/testbench/scripts/analysis/convert_raw.py
@@ -13,13 +13,52 @@ BSIZE_SP = 512 # Max size of a line of data; we don't want to read the
 MDATA_LIST = [b'title', b'date', b'plotname', b'flags', b'no. variables',
               b'no. points', b'dimensions', b'command', b'option']
 
-def D(xlist,ylist):
-    yprime = np.diff(ylist)/np.diff(xlist)
-    xprime=[]
-    for i in range(len(yprime)):
-        xtemp = (xlist[i+1]+xlist[i])/2
-        xprime = np.append(xprime,xtemp)
-    return xprime, yprime
+def t_meas(x_axis, y_axis, thresh, occur_num, edge_type):
+
+    arr__temp = np.sign(np.subtract (y_axis, thresh*np.ones(len(y_axis))))
+    if (   ((1  not in np.sign(np.diff(y_axis)))  and  edge_type =='rise')
+        or ((-1 not in np.sign(np.diff(y_axis)))  and  edge_type =='fall')
+        or (thresh > np.max(y_axis))
+        or (thresh < np.min(y_axis))):
+
+        print('t_meas function error: NO CROSSING OF SPECIFIED TYPE HAPENS')
+        return(False,0)
+
+    if (edge_type == 'rise'):
+        arr_dn = np.delete(arr__temp,len(arr__temp)-1)
+        arr_up = np.delete(arr__temp,0)
+        rise_idx = np.where(((arr_up == 1) & (arr_dn == -1)) | ((arr_up == 1) & (arr_dn == 0)))[0]
+
+        t_dn = x_axis[rise_idx[occur_num-1]]
+        t_up = x_axis[rise_idx[occur_num-1]+1]
+
+        y_dn = y_axis[rise_idx[occur_num-1]]
+        y_up = y_axis[rise_idx[occur_num-1]+1]
+
+    elif(edge_type == 'fall'):
+        arr_up = np.delete(arr__temp,len(arr__temp)-1)
+        arr_dn = np.delete(arr__temp,0)
+        fall_idx = np.where(((arr_up == 1) & (arr_dn == -1)) | ((arr_up == 1) & (arr_dn == 0)))[0]
+
+        t_up = x_axis[fall_idx[occur_num-1]]
+        t_dn = x_axis[fall_idx[occur_num-1]+1]
+
+        y_up = y_axis[fall_idx[occur_num-1]]
+        y_dn = y_axis[fall_idx[occur_num-1]+1]
+    else:
+        print("t_meas function error: WRONG EDGE TYPE")
+        return(False,0)
+
+
+    if (thresh == y_up):
+        t_cross = t_up
+    elif (thresh == y_dn):
+        t_cross = t_dn
+    else:
+        slope = (y_up - y_dn)/(t_up - t_dn)
+        t_cross = t_dn + (thresh - y_dn)/slope
+    
+    return(True,t_cross)
 
 def rawread(fname: str):
     """Read ngspice binary raw files. Return tuple of the data, and the
@@ -91,32 +130,38 @@ def plotvec(ax,x,y,plabel,xlabel,ylabel,title,xmin,xmax,ymin,ymax):
     # ax.set_ylim(ylimt)
 
 if __name__ == '__main__':
-    arrs, plots = rawread('PLL.raw')
+    arrs, plots = rawread('../../pll.raw')
     plt.figure(1)
-    plt.plot(arrs[1]["time"], arrs[1]["v(vctrl)"])
+    plt.plot(arrs[1]["time"], arrs[1]["v(xpll.vctrl)"])
     plt.xlabel('time (sec)') 
     plt.ylabel('Amplitude (V)')
     plt.title('Vcontrol')
     plt.grid(True)
-    plt.savefig('vctrl.png')
+    plt.savefig('../../../results/vctrl.png')
+
+
     plt.figure(2)
-    plt.plot(arrs[1]["time"], arrs[1]["i(vtest)"])
-    plt.xlabel('time (sec)') 
-    plt.ylabel('Current (A)')
-    plt.title('Charge pump current')
-    plt.savefig('Icp.png')
-    plt.grid(True)
-
-    control_list_prime, kvco = D(control_list, freq_list)
-
-    plt.plot(control_list_prime, kvco*1e+3,linewidth = 3, color = 'black',linestyle='dashed',label=df["corner name"][itr])
-
-    plt.figure(3)
-    plt.plot(arrs[1]["time"], arrs[1]["v(vp)"])
+    plt.plot(arrs[1]["time"], arrs[1]["v(vco_out)"])
     plt.xlabel('time (sec)') 
     plt.ylabel('Amplitude (V)')
-    plt.title('VCO Output')
-    plt.savefig('VCO out.png')
+    plt.title('VCO Output after the inverter')
+    plt.savefig('../../../results/VCO_out_after_inv.png')
+    plt.grid(True)
+
+    # plt.figure(2)
+    # plt.plot(arrs[1]["time"], arrs[1]["i(xpll.vtest)"])
+    # plt.xlabel('time (sec)') 
+    # plt.ylabel('Current (A)')
+    # plt.title('Charge pump current')
+    # plt.savefig('../../../results/Icp.png')
+    # plt.grid(True)
+
+    plt.figure(3)
+    plt.plot(arrs[1]["time"], arrs[1]["v(xpll.vp)"])
+    plt.xlabel('time (sec)') 
+    plt.ylabel('Amplitude (V)')
+    plt.title('VCO Output before the inverter')
+    plt.savefig('../../../results/VCO_out_before_inv.png')
     plt.grid(True)
 
     plt.figure(4)
@@ -129,7 +174,7 @@ if __name__ == '__main__':
     plt.grid(True)
 
     plt.subplot(4,1,2)
-    plt.plot(arrs[1]["time"] , arrs[1]["v(fb)"], color='g', label='FB')
+    plt.plot(arrs[1]["time"] , arrs[1]["v(xpll.fb)"], color='g', label='FB')
     plt.xlabel('time (sec)') 
     plt.ylabel('Amplitude (V)')
     plt.title('FB')
@@ -137,7 +182,7 @@ if __name__ == '__main__':
     plt.grid(True)
     
     plt.subplot(4,1,3)
-    plt.plot(arrs[1]["time"] , arrs[1]["v(up)"] ,color='r', label='UP')
+    plt.plot(arrs[1]["time"] , arrs[1]["v(xpll.up)"] ,color='r', label='UP')
     plt.xlabel('time (sec)') 
     plt.ylabel('Amplitude (V)')
     plt.title('UP')
@@ -145,7 +190,7 @@ if __name__ == '__main__':
     plt.grid(True)
     
     plt.subplot(4,1,4)
-    plt.plot(arrs[1]["time"] , arrs[1]["v(dn)"] , color='c', label='DWN')
+    plt.plot(arrs[1]["time"] , arrs[1]["v(xpll.dn)"] , color='c', label='DWN')
     plt.xlabel('time (sec)') 
     plt.ylabel('Amplitude (V)')
     plt.title('DWN')
@@ -153,7 +198,7 @@ if __name__ == '__main__':
     plt.grid(True)
     plt.legend()
     plt.suptitle("Before lock")
-    plt.savefig('beforelock.png')
+    plt.savefig('../../../results/beforelock.png')
 
     plt.figure(5)
     plt.subplot(4,1,1)
@@ -161,35 +206,38 @@ if __name__ == '__main__':
     plt.xlabel('time (sec)') 
     plt.ylabel('Amplitude (V)')
     plt.title('REF')
-    plt.xlim(210e-6, 210.4e-6)
+    plt.xlim(26e-6, 26.4e-6)
     plt.grid(True)
 
     plt.subplot(4,1,2)
-    plt.plot(arrs[1]["time"] , arrs[1]["v(fb)"], color='g', label='FB')
+    plt.plot(arrs[1]["time"] , arrs[1]["v(xpll.fb)"], color='g', label='FB')
     plt.xlabel('time (sec)') 
     plt.ylabel('Amplitude (V)')
     plt.title('FB')
-    plt.xlim(210e-6, 210.4e-6)
+    plt.xlim(26e-6, 26.4e-6)
     plt.grid(True)
     
     plt.subplot(4,1,3)
-    plt.plot(arrs[1]["time"] , arrs[1]["v(up)"] ,color='r', label='UP')
+    plt.plot(arrs[1]["time"] , arrs[1]["v(xpll.up)"] ,color='r', label='UP')
     plt.xlabel('time (sec)') 
     plt.ylabel('Amplitude (V)')
     plt.title('UP')
-    plt.xlim(210e-6, 210.4e-6)
+    plt.xlim(26e-6, 26.4e-6)
     plt.grid(True)
     
     plt.subplot(4,1,4)
-    plt.plot(arrs[1]["time"] , arrs[1]["v(dn)"] , color='c', label='DWN')
+    plt.plot(arrs[1]["time"] , arrs[1]["v(xpll.dn)"] , color='c', label='DWN')
     plt.xlabel('time (sec)') 
     plt.ylabel('Amplitude (V)')
     plt.title('DWN')
-    plt.xlim(210e-6, 210.4e-6)
+    plt.xlim(26e-6, 26.4e-6)
     plt.grid(True)
     plt.legend()
     plt.suptitle("after lock")
-    plt.savefig('aferlock.png')
+    plt.savefig('../../../results/aferlock.png')
+
+  
+    
     # fig, ((ax1), (ax2), (ax3), (ax4)) = plt.subplots(nrows=4, ncols=1)
     # #plotvec(ax,x,y,plabel,xlabel,ylabel,title,legendx,xlimt,ylimt):
     # plotvec(ax1,arrs[1]["time"], arrs[1]["v(ref)"],'REF','Time(Sec)','Amplitude(V)','Ref signal',1e-6,1.4e-6,0,1.9)
@@ -200,10 +248,10 @@ if __name__ == '__main__':
     # plt.savefig('beforelock.png')
     # fig2, ((ax1), (ax2), (ax3), (ax4)) = plt.subplots(nrows=4, ncols=1)
     # #plotvec(ax,x,y,plabel,xlabel,ylabel,title,legendx,xlimt,ylimt):
-    # plotvec(ax1,arrs[1]["time"], arrs[1]["v(ref)"],'REF','Time(Sec)','Amplitude(V)','Ref signal',210e-6,210.4e-6,0,1.9)
-    # plotvec(ax2,arrs[1]["time"], arrs[1]["v(fb)"],'FB','Time(Sec)','Amplitude(V)','FB signal',210e-6,210.4e-6,0,1.9)
-    # plotvec(ax3,arrs[1]["time"], arrs[1]["v(up)"],'UP','Time(Sec)','Amplitude(V)','UP signal',210e-6,210.4e-6,0,1.9)
-    # plotvec(ax4,arrs[1]["time"], arrs[1]["v(dn)"],'DWN','Time(Sec)','Amplitude(V)','DWN signal',210e-6,210.4e-6,0,1.9)
+    # plotvec(ax1,arrs[1]["time"], arrs[1]["v(ref)"],'REF','Time(Sec)','Amplitude(V)','Ref signal',26e-6,26.4e-6,0,1.9)
+    # plotvec(ax2,arrs[1]["time"], arrs[1]["v(fb)"],'FB','Time(Sec)','Amplitude(V)','FB signal',26e-6,26.4e-6,0,1.9)
+    # plotvec(ax3,arrs[1]["time"], arrs[1]["v(up)"],'UP','Time(Sec)','Amplitude(V)','UP signal',26e-6,26.4e-6,0,1.9)
+    # plotvec(ax4,arrs[1]["time"], arrs[1]["v(dn)"],'DWN','Time(Sec)','Amplitude(V)','DWN signal',26e-6,26.4e-6,0,1.9)
     # plt.suptitle("After lock")
     # plt.savefig('afterlock.png')
 
@@ -211,3 +259,16 @@ if __name__ == '__main__':
 # Local Variables:
 # mode: python
 # End:
+
+check1,t1 = t_meas(arrs[1]["time"] , arrs[1]["v(xpll.fb)"], 1, 200, 'rise')
+check2,t2 = t_meas(arrs[1]["time"] , arrs[1]["v(xpll.fb)"], 1, 201, 'rise')
+freq_fb = 1/(t2-t1)
+
+check1,t1 = t_meas(arrs[1]["time"] , arrs[1]["v(vco_out)"], 1, 71000, 'rise')
+check2,t2 = t_meas(arrs[1]["time"] , arrs[1]["v(vco_out)"], 1, 71001, 'rise')
+freq_vco = 1/(t2-t1)
+n = freq_vco/freq_fb
+
+print("vco_freq(Ghz):",freq_vco/1e9)
+print("divider out freq (Mhz):",freq_fb/1e6)
+print("division ratio:",n)
